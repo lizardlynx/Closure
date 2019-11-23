69f51231755a3a49cd1bad2536f8949d2ddd75db
liza <gorbunova.elizaveta@gmail.com>
2019-11-24 01:05:02 +0200

    Done

--- Exercises/1-seq.js
+++ Exercises/1-seq.js
@@ -1,5 +1,7 @@
 'use strict';
 
-const seq = f => g => x => 0;
+const seq = (...args) => g => ((typeof g === 'number') ?
+  args.reverse().reduce(((f, n) => n(f)), g) :
+  seq(...args.concat(g)));
 
 module.exports = { seq };
